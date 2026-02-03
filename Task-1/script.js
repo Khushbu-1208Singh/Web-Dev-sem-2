8b16c5b308634bc25e4d27081b97679df08bbd93
Khushbu Singh <khushbusingh@Khushbus-MacBook-Air.local>
2026-02-03 12:35:50 +0530

    task  1

--- Task-1/script.js
+++ Task-1/script.js
@@ -7,10 +7,37 @@ profile.style.textAlign="center";
 //change text color
 //increase font size 
 // print total count in console.
-const highlight = document.getElementsByClassName("important");
-console.log(highlight)
-for(let i=0;i<highlight.length;i++){
-    highlight[i].style.color = "blue"
-    highlight[i].style.fontsize = "20px"
+// const highlight = document.getElementsByClassName("important");
+// console.log(highlight)
+// for(let i=0;i<highlight.length;i++){
+//     highlight[i].style.color = "blue"
+//     highlight[i].style.fontsize = "20px"
+// }
+//Task-3 -> get element by tag name make even pragraph blue odd paragraph red and bold the last paragraph.
+/*const task3 = document.getElementsByTagName("p");
+console.log(task3);
+for(let i = 0; i < task3.length; i++) {
+    if(i % 2 === 0) {
+        task3[i].style.color = "blue";
+    } 
+    else{
+        task3[i].style.color = "green";
+    }
 }
-console.log 
+task3[task3.length - 1].style.fontWeight = "bold";*/
+//task 4 -> first match modifier 
+/*const task4 = document.querySelectorAll(".box");
+for(let i=0;i<task4.length;i++){
+    task4[i].style.backgroundColor="cyan";
+}
+task4.textContent="I am first box"*/
+// task 5-> nested paragraph 
+/*const section=document.getElementById("content");
+const task5=section.querySelectorAll("p");
+for(let i=0;i<task5.length;i++){
+    task5[i].style.color="purple"
+}*/
+
+
+
+
